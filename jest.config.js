b71fcc1ac3b6c18e3127859be5e4ad18784e2a8f
santiseijas <santiago.seijas@eccocar.com>
2022-07-05 00:27:02 +0200

    fix lint

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-   preset: 'react-native',
-   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-   transformIgnorePatterns: [
-    'node_modules/(?!@react-native|react-native|@react-navigation)'
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!@react-native|react-native|@react-navigation)',
   ],
-  
- };
+};
--- src/config/setup.js
+++ src/config/setup.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler/jestSetup';
+import jest from 'jest';
 
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
