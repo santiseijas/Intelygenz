b3fface3941a80fdf12c61faeabe70c9fd1fd668
santiseijas <santiago.seijas@eccocar.com>
2022-07-05 00:14:53 +0200

    commit Filter test

---
+++ __tests__/Filter-test.js
@@ -0,0 +1,22 @@
+//test for Filter component with searchText and searchFilterFunction
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import Filter from '../src/components/Filter';
+
+describe('Filter component', () => {
+  it('should render correctly', () => {
+    const {getByPlaceholderText} = render(<Filter />);
+    const textInput = getByPlaceholderText('Search');
+    expect(textInput).toBeTruthy();
+  });
+
+  it('should call searchFilterFunction when text is entered', () => {
+    const searchFilterFunction = jest.fn();
+    const {getByPlaceholderText} = render(
+      <Filter searchFilterFunction={searchFilterFunction} />,
+    );
+    const textInput = getByPlaceholderText('Search');
+    fireEvent.changeText(textInput, 'test');
+    expect(searchFilterFunction).toHaveBeenCalled();
+  });
+});
